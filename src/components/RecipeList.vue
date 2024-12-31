e00a4a9fc0ae965792d66865152d14fb15ed5e28
Kaloyan Parvanov <kaloyanparvanov00@gmail.com>
2024-12-31 13:40:27 +0200

    add recipe list component

---
+++ src/components/RecipeList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <RecipeCard
+      v-for="(recipe, index) in recipes"
+      :key="index"
+      :recipe="recipe"
+    />
+  </div>
+  <!-- No Recipes Message -->
+  <p v-if="recipes.length === 0" class="text-amber-100/80 text-center mt-12">
+    You have no recipes yet. Start by creating one!
+  </p>
+</template>
+<script setup lang="ts">
+import type { Recipe } from "../interfaces/recipe";
+import RecipeCard from "../components/RecipeCard.vue";
+
+defineProps<{ recipes: Recipe[] }>();
+</script>
+<style scoped></style>
