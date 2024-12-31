c5f1e33eddfa27a7d95cdfbdb22607be152cb5ab
Denis Spasov <denis_spasov@abv.bg>
2024-12-31 14:01:22 +0200

    fix recipe image not rendering correctly on filter

--- src/components/RecipeList.vue
+++ src/components/RecipeList.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
     <RecipeCard
-      v-for="(recipe, index) in recipes"
-      :key="index"
+      v-for="(recipe) in recipes"
+      :key="recipe.id"
       :recipe="recipe"
     />
   </div>
